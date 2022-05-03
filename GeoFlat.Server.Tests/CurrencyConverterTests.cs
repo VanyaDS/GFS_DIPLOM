206e218c985d81c230e3c2b3519a29a0c3f81a15
Ivan Denisyuk <ivandenisyuk2015@gmail.com>
2022-05-03 21:47:42 +0300

    Add CurrencyConverterTests

---
+++ GeoFlat.Server.Tests/CurrencyConverterTests.cs
@@ -0,0 +1,31 @@
+﻿using GeoFlat.Server.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GeoFlat.Server.Tests
+{
+    public class CurrencyConverterTests
+    {
+        [Fact]
+        public void ConvertFromUSDToBYNPriceUSDCorrectResult()
+        {
+            //Arrange
+            CurrencyConverter converter = new CurrencyConverter
+            {
+                USD = 2,
+                BYN = 5
+            };
+            int ParamPriceUSD = 10;
+            int exptectedResultPrice = 4;
+            //Act
+            int result = converter.ConvertFromUSDToBYN(ParamPriceUSD);
+
+            //Assert          
+            Assert.Equal(exptectedResultPrice, result);
+        }
+    }
+}
